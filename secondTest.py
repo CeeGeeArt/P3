2a3f204617a9df78286b8edb525a91c0e11e2e7e
Arthrangos <Arthrangos@gmail.com>
2018-10-10 13:22:54 +0200

    Line testing

--- secondTest.py
+++ secondTest.py
@@ -4,3 +4,6 @@ import cv2
 img = np.zeros((512, 512, 3), np.uint8)
 
 cv2.line(img,(0, 0), (511, 511), (255, 0, 0), 5)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
