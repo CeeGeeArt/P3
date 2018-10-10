e94cad1915e65f1e9bac1476080cf9a51b0067fc
Caseguy <33653535+Caseguy@users.noreply.github.com>
2018-10-10 14:24:16 +0200

    Fixed it for you Christian. Program shows a line that goes form 0,0 - 511,511

--- secondTest.py
+++ secondTest.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
 
-img = np.zeros((512, 512, 3), np.uint8)
 
-cv2.line(img,(0, 0), (511, 511), (255, 0, 0), 5)
+img = np.zeros((512, 512,3), np.uint8)
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+cv2.line(img,(0,0),(511,511),(0,255,0),5)
+cv2.line(img,(0,0),(511,511),(0,255,0),5)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
