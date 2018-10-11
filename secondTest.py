80468c1d54f3e1850e90fc0908a852e3070fc682
Arthrangos <Arthrangos@gmail.com>
2018-10-11 10:53:37 +0200

    Line testing

--- secondTest.py
+++ secondTest.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 
 
-img = np.zeros((512, 512,3), np.uint8)
+img = np.zeros((750, 750), np.uint8)
 
-cv2.line(img,(0,0),(511,511),(0,255,0),5)
-cv2.line(img,(0,0),(511,511),(0,255,0),5)
+cv2.line(img, (0, 0), (511, 511), (0, 255, 0), 5)
+cv2.line(img, (0, 0), (511, 511), (0, 255, 0), 5)
 
 cv2.imshow('image',img)
 cv2.waitKey(0)
