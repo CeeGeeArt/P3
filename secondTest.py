3a9701fb6f7be2d47520a909aebed56581a20976
Arthrangos <Arthrangos@gmail.com>
2018-10-11 11:21:12 +0200

    Line testing

--- secondTest.py
+++ secondTest.py
@@ -2,11 +2,12 @@ import numpy as np
 import cv2
 
 
-img = np.zeros((750, 750), np.uint8)
+img = np.zeros((1000, 1000, 3), np.uint8)
 
-cv2.line(img, (0, 0), (511, 511), (0, 255, 0), 5)
-cv2.line(img, (0, 0), (511, 511), (0, 255, 0), 5)
+cv2.line(img, (250, 0), (250, 750), (0, 255, 0), 5)
+cv2.line(img, (250, 750), (750, 750), (0, 255, 0), 5)
+cv2.line(img, (750, 750), (750, 0), (0, 255, 0), 5)
 
-cv2.imshow('image',img)
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
