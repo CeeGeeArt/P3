fcc06a0d7ef811314d783b6eb74351247a1e2544
HamPaa35 <mfaldt17@student.aau.dk>
2018-10-25 11:33:01 +0200

    Started making some class code for a rectangle

--- laserPhysicsTest.py
+++ laserPhysicsTest.py
@@ -2,13 +2,30 @@ import numpy as np
 import cv2
 import time
 
+img = cv2.imread('test.jpg')
+
+class rectangle:
+    def __init__(self, x1, y1, x2, y2, img):
+        topLCorner = (x1, y1)
+        bottomRCorner = (x2, y2)
+        cv2.rectangle(img, topLCorner, bottomRCorner, (0, 255, 0), 2)
+
+    def collisionleft(self, ):
+
+
+        intersectionY = ((((x1 * y2) - (y1 * x2)) * (y3 - y4)) - (y1 - y2) * ((x3 * y4) - (y3 * x4))) \
+                        / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)))
+
+
+
+
 def laserFire(videoFeed, totLaserPos, timePerPos):
     vRow, vCol, vCH = videoFeed.shape
     lsX = 100
     lsY = int(vRow/2)
     leX = int(vCol-50)
     leY = int(vRow/2)+200
-    osX = int(vCol/2)
+    osX = int(vCol/3)
     osY = int(vRow/2)-100
     oeX = vCol-100
     oeY = int(vRow/2)+100
@@ -20,10 +37,11 @@ def laserFire(videoFeed, totLaserPos, timePerPos):
     y2 = leY
     y3 = osY
     y4 = oeY
-    intersectionX=-1*((lsX-leX)*(osX*oeY-osX*osY)-)
-    intersectionY=((((x1*y2)-(y1*x2))*(y3-y4))-(y1-y2)*((x3*y4)-(y3*x4)))/(((x1-x2)*(y3-y4))-((y1-y2)*(x3-x4)))
-    drawObj = cv2.rectangle(videoFeed, (osX,osY), (oeX, oeY), (0, 255, 0), 2)
-    drawLaser = cv2.line(videoFeed, (lsX, lsY), (lsX, lsY), (0, 0, 255, 127), 5)
+    intersectionX = ((((lsX * leY) - (lsX * leX)) * (osX - osX)) - ((lsX - leX) * ((osX * oeY) - (osY * osX)))) \
+                    / (((lsX - leX) * (osY - oeY)) - ((lsY - leY * (osX - osX))))
+    rectangle(osX, osY, oeX, oeY, videoFeed)
+    drawLaser = cv2.line(videoFeed, (lsX, lsY), (leX, leY), (0, 0, 255), 5)
+    drawLaser = cv2.line(videoFeed, (int(intersectionX), int(intersectionY)), (int(intersectionX), int(intersectionY)), (255, 0, 0), 5)
 
 
 cap = cv2.VideoCapture(0)
