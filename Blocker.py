efbb93d188285e74f0e062316a708ff5d0535863
HamPaa35 <mfaldt17@student.aau.dk>
2018-10-29 13:32:27 +0100

    Added classes

--- Blocker.py
+++ Blocker.py
@@ -1,3 +1,7 @@
 import Collision
+import Laser
+import Target
+import Mirror
+
 class Blocker(Collision):
     def __init__(self):
--- Mirror.py
+++ Mirror.py
@@ -1,3 +1,7 @@
 import Collision
+import Laser
+import Target
+import Blocker
+
 class Mairror(Collision):
     def __init(self):
