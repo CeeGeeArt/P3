56c359861b16c47c97894269481c37d3dfc8e37e
Arthrangos <Arthrangos@gmail.com>
2018-10-29 13:42:51 +0100

    A frame counter

---
+++ blobDetection.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np;
+im = cv2.imread('blob.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 50
+params.maxThreshold = 150
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 150
+detector = cv2.SimpleBlobDetector_create(params)
+
+# Detect blobs.
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+# the size of the circle corresponds to the size of blob
+
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show blobs
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
