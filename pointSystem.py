184d47f1a2e8cf20d8ad1a46ebfbd1a1d467b500
Yeehaaman <36846857+Yeehaaman@users.noreply.github.com>
2018-11-02 09:57:45 +0100

    Point

--- pointSystem.py
+++ pointSystem.py
@@ -1,5 +1,19 @@
 import cv2
+import numpy as np
 
 player1Point = 0
 player2Point = 0
+
+rectX1 = 490
+rectY1 = 310
+rectX2 = 640
+rectY2 = 460
+img = np.zeros((462, 642, 3), np.uint8)
+
+rectangle = cv2.rectangle(img, (rectX1, rectY1), (rectX2, rectY2), (150, 150, 150), 4)
+line1 = cv2.line(img, (0, 350), (510, 350), (255, 0, 0), 3)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
