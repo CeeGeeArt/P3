e68ab14a91ec381c9abf08d843426145f3384ac5
HamPaa35 <mfaldt17@student.aau.dk>
2018-11-08 11:41:57 +0100

    Made the variables easier to read

--- Mirror.py
+++ Mirror.py
@@ -71,77 +71,76 @@ def angleDetermine(laserX1, laserY1, laserX2, laserY2, closestCollisionX, closes
     #return Laser.Laser(cx, cy, int(reflectionEndX), int(reflectionEndY), img), 0
 
 
-def angleDetermineStillBad(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2, img):
-    vectorAx = ax2 - ax1
-    vectorAy = ay2 - ay1
-    vectorBx = bx2 - bx1
-    vectorBy = by2 - by1
-    vectorCx = vectorAx
-    vectorCy = vectorAy
-
-    vectorA = [vectorAx, vectorAy]
-    vectorB = [vectorBx, vectorBy]
-    vectorB90deg = [-vectorBy, vectorBx]
-    normalVectorToBx1= ax2
-    normalVectorToBx2= ax2+vectorB90deg[0]
-    normalVectorToBy1= ay2
-    normalVectorToBy2= ay2+vectorB90deg[1]
-    normalVectorToBx = normalVectorToBx2-normalVectorToBx1
-    normalVectorToBy= normalVectorToBy2-normalVectorToBy1
-    cv2.line(img, (ax2, ay2), (ax2+vectorCx, ay2+vectorCy), (255, 255, 0), 5)
-    magnitudeOfA = int(math.sqrt(math.pow(vectorAx, 2) + math.pow(vectorAy, 2))) #POP POP!!! #https://www.youtube.com/watch?v=dyp9Qw12boI&ab_channel=Jalkie
-    magnitudeOfB = int(math.sqrt(math.pow(vectorBx, 2) + math.pow(vectorBy, 2)))
-    #magnitudeOfnormalB = int(math.sqrt(math.pow(normalVectorToBx, 2) + math.pow(normalVectorToBy, 2)))
-    #normOfnormalVectorToBx = normalVectorToBx/magnitudeOfnormalB
-    #normOfnormalVectorToBy = normalVectorToBy/magnitudeOfnormalB
-
-    #dotProductOfStuff = (ax2*normalVectorToBx)+(ay2*normalVectorToBy)
-
-    #rLx = vectorAx-2*(dotProductOfStuff)*normOfnormalVectorToBx
-    #rLy = vectorAy - 2 * (dotProductOfStuff) * normOfnormalVectorToBy
-
-
-
-    vectorProduct = (vectorAx*vectorBx) + (vectorAy*vectorBy)
-    magnitudeProduct = (magnitudeOfA * magnitudeOfB)
-
-
-
-def angleDetermineBad(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2, img):
-    vectorAx = ax2 - ax1
-    vectorAy = ay2 - ay1
-    vectorBx = bx2 - bx1
-    vectorBy = by2 - by1
-
-    vectorA = [vectorAx, vectorAy]
-    vectorB = [vectorBx, vectorBy]
-    magnitudeOfA = int(math.sqrt(math.pow(vectorAx, 2) + math.pow(vectorAy, 2))) #POP POP!!! #https://www.youtube.com/watch?v=dyp9Qw12boI&ab_channel=Jalkie
-    magnitudeOfB = int(math.sqrt(math.pow(vectorBx, 2) + math.pow(vectorBy, 2)))
-
-    print(vectorAx, vectorAy, vectorBx, vectorBy, magnitudeOfA, magnitudeOfB)
-
-    vectorProduct = (vectorAx*vectorBx) + (vectorAy*vectorBy)
-    magnitudeProduct = (magnitudeOfA * magnitudeOfB)
-    print(vectorProduct, magnitudeProduct)
-
-    cosAngle = vectorProduct/magnitudeProduct
-    print("cos",cosAngle)
-    Angle = math.degrees(math.acos(cosAngle))
-    print("A",Angle)
-
-    print(vectorA, vectorB)
-    cross = np.cross(vectorA, vectorB)
-    print("cross", cross)
-
-    x1 = ax2
-    y1 = ay2
-    rLx2 = x1 + (magnitudeOfA * math.cos(Angle))
-    rLy2 = y1 + (magnitudeOfA * math.sin(Angle))
-    print("rLx2", rLx2)
-    print("rLy2", rLy2)
-
-    print("final shit",ax2, ay2, rLx2, rLy2)
-    cv2.line(img, (-vectorBy, vectorBx), (vectorBy, -vectorBx), (0, 255, 0), 5)
-    cv2.line(img, (int(ax2), int(ay2)), (int(rLx2), int(rLy2)), (0, 255, 0), 5)
-    #Laser.Laser
-5
+# def angleDetermineStillBad(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2, img):
+#     vectorAx = ax2 - ax1
+#     vectorAy = ay2 - ay1
+#     vectorBx = bx2 - bx1
+#     vectorBy = by2 - by1
+#     vectorCx = vectorAx
+#     vectorCy = vectorAy
+#
+#     vectorA = [vectorAx, vectorAy]
+#     vectorB = [vectorBx, vectorBy]
+#     vectorB90deg = [-vectorBy, vectorBx]
+#     normalVectorToBx1= ax2
+#     normalVectorToBx2= ax2+vectorB90deg[0]
+#     normalVectorToBy1= ay2
+#     normalVectorToBy2= ay2+vectorB90deg[1]
+#     normalVectorToBx = normalVectorToBx2-normalVectorToBx1
+#     normalVectorToBy= normalVectorToBy2-normalVectorToBy1
+#     cv2.line(img, (ax2, ay2), (ax2+vectorCx, ay2+vectorCy), (255, 255, 0), 5)
+#     magnitudeOfA = int(math.sqrt(math.pow(vectorAx, 2) + math.pow(vectorAy, 2))) #POP POP!!! #https://www.youtube.com/watch?v=dyp9Qw12boI&ab_channel=Jalkie
+#     magnitudeOfB = int(math.sqrt(math.pow(vectorBx, 2) + math.pow(vectorBy, 2)))
+#     #magnitudeOfnormalB = int(math.sqrt(math.pow(normalVectorToBx, 2) + math.pow(normalVectorToBy, 2)))
+#     #normOfnormalVectorToBx = normalVectorToBx/magnitudeOfnormalB
+#     #normOfnormalVectorToBy = normalVectorToBy/magnitudeOfnormalB
+#
+#     #dotProductOfStuff = (ax2*normalVectorToBx)+(ay2*normalVectorToBy)
+#
+#     #rLx = vectorAx-2*(dotProductOfStuff)*normOfnormalVectorToBx
+#     #rLy = vectorAy - 2 * (dotProductOfStuff) * normOfnormalVectorToBy
+#
+#
+#
+#     vectorProduct = (vectorAx*vectorBx) + (vectorAy*vectorBy)
+#     magnitudeProduct = (magnitudeOfA * magnitudeOfB)
+#
+#
+#
+# def angleDetermineBad(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2, img):
+#     vectorAx = ax2 - ax1
+#     vectorAy = ay2 - ay1
+#     vectorBx = bx2 - bx1
+#     vectorBy = by2 - by1
+#
+#     vectorA = [vectorAx, vectorAy]
+#     vectorB = [vectorBx, vectorBy]
+#     magnitudeOfA = int(math.sqrt(math.pow(vectorAx, 2) + math.pow(vectorAy, 2))) #POP POP!!! #https://www.youtube.com/watch?v=dyp9Qw12boI&ab_channel=Jalkie
+#     magnitudeOfB = int(math.sqrt(math.pow(vectorBx, 2) + math.pow(vectorBy, 2)))
+#
+#     print(vectorAx, vectorAy, vectorBx, vectorBy, magnitudeOfA, magnitudeOfB)
+#
+#     vectorProduct = (vectorAx*vectorBx) + (vectorAy*vectorBy)
+#     magnitudeProduct = (magnitudeOfA * magnitudeOfB)
+#     print(vectorProduct, magnitudeProduct)
+#
+#     cosAngle = vectorProduct/magnitudeProduct
+#     print("cos",cosAngle)
+#     Angle = math.degrees(math.acos(cosAngle))
+#     print("A",Angle)
+#
+#     print(vectorA, vectorB)
+#     cross = np.cross(vectorA, vectorB)
+#     print("cross", cross)
+#
+#     x1 = ax2
+#     y1 = ay2
+#     rLx2 = x1 + (magnitudeOfA * math.cos(Angle))
+#     rLy2 = y1 + (magnitudeOfA * math.sin(Angle))
+#     print("rLx2", rLx2)
+#     print("rLy2", rLy2)
+#
+#     print("final shit",ax2, ay2, rLx2, rLy2)
+#     cv2.line(img, (-vectorBy, vectorBx), (vectorBy, -vectorBx), (0, 255, 0), 5)
+#     cv2.line(img, (int(ax2), int(ay2)), (int(rLx2), int(rLy2)), (0, 255, 0), 5)
+#     #Laser.Laser
