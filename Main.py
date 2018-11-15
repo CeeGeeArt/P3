01dde389915a97967a1e00390c3ed8746f145657
HamPaa35 <mfaldt17@student.aau.dk>
2018-11-15 12:32:00 +0100

    Minor changes to the code, to check what went wrong

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import Collision
 import Laser
 import Blocker
 import cv2
+import Queue
 
 
 img = cv2.imread('test.jpg')
@@ -29,42 +30,99 @@ def laserFire(videoFeed, totLaserPos, timePerPos):
     #reflectedLaser2 = col3.collisionDetection(img)
     #col4 = Collision.Collision(testBlocker, reflectedLaser2)
     #reflectedLaser3 = col4.collisionDetection(img)
-    ##reflectedLaser3.drawLaser()
-
-
+    #reflectedLaser3.drawLaser()
 
 
     testMirror = Mirror.Mirror(300, 400, 900, 1100, img)
-    testBlocker = Mirror.Mirror(100, 600, 200, 900, img)
+    #testMirror2 = Mirror.Mirror(100, 600, 200, 900, img)
     #testBlocker = Mirror.Mirror(1000, 400, 1100, 500, img)
 
-    # originalLaser = Laser.Laser(lsX, lsY, leX, leY, img)
+    originalLaser = Laser.Laser(lsX, lsY, leX, leY, img)
+
+    col = Collision.Collision(testMirror, originalLaser)
+
+    outPutLaser = col.collisionDetection(img)
     #
-    # col = Collision.Collision(testMirror, originalLaser)
-    #
-    # outPutLaser = col.collisionDetection(img)
-    #
-    # outPutLaser.drawLaser()
+    outPutLaser.drawLaser()
 
-    laserArray = [Laser.Laser(lsX, lsY, leX, leY, img)]
-    colidedLasers = []
-    blockerMirrorArray = [testBlocker,testMirror]
-
-    for i in range(len(laserArray)):
-        for j in range(len(blockerMirrorArray)):
-            col = Collision.Collision(blockerMirrorArray[j],laserArray[i])
-            tempLaser = col.collisionDetection(img)
-            colidedLasers.append(tempLaser)
-
-        colidedLasers[i].drawLaser()
-
-
-
-
-
-
-
-
+#     laserArray = []
+#     colidedLasers = []
+#     blockerMirrorArray = [testMirror2, testMirror]
+#     laserArray = [Laser.Laser(lsX, lsY, leX, leY, img)]
+#     laserCollisionState = 1
+#     collidedLasers = []
+#
+#
+#     for i in range(len(laserArray)):
+#         for j in range(len(blockerMirrorArray)):
+#             col = Collision.Collision(blockerMirrorArray[j], laserArray[i])
+#             tempLaser, collissionState = col.collisionDetection(img)
+#             if collissionState == 1:
+#                 laserArray.append(tempLaser)
+#                 colidedLasers.append(tempLaser)
+#
+#     # if prevArrayLength<len(laserArray):
+#     #     for i in range(1):
+#     #         for j in range(len(blockerMirrorArray)):
+#     #             col = Collision.Collision(blockerMirrorArray[1], laserArray[i])
+#     #             tempLaser, collissionState = col.collisionDetection(img)
+#     #             if collissionState == 1:
+#     #                 laserArray.append(tempLaser)
+#     #                 colidedLasers.append(tempLaser)
+#     #                 print(len(laserArray))
+#     # for i in range(len(colidedLasers)):
+#     #     colidedLasers[i].drawLaser()
+#
+#     # for laser in laserArray:
+#     #     for Blocker in blockerMirrorArray:
+#     #         col = Collision.Collision(Blocker, laser)
+#     #         tempLaser, colState = col.collisionDetection(img)
+#     #         laserArray.append(tempLaser)
+#     #         if colState == 1:
+#     #             colidedLasers.append(tempLaser)
+#     #
+#     # for laser in colidedLasers:
+#     #     laser.drawLaser()
+#
+#     # laserArray = []
+#     # colidedLasers = []
+#     # blockerMirrorArray = [testMirror2, testMirror]
+#     # laserArray = [Laser.Laser(lsX, lsY, leX, leY, img)]
+#     # laserCollisionState = 1
+#     #
+#     # while(laserCollisionState == 1):
+#     #     for i in range(len(laserArray)):
+#     #         for j in range(len(blockerMirrorArray)):
+#     #             col = Collision.Collision(blockerMirrorArray[1], laserArray[i])
+#     #             tempLaser, laserCollisionState = col.collisionDetection(img)
+#     #             if laserCollisionState == 1:
+#     #                 laserArray.append(tempLaser)
+#     #                 colidedLasers.append(tempLaser)
+#
+#
+#     originalLaser = Laser.Laser(0,0,200,200,img)
+#     blockerMirrorArray = [testMirror2, testMirror]
+#     laserQueue = Queue.Queue()
+#     laserQueue.put(originalLaser)
+#     collidedLasers = []
+#
+#
+#     while not laserQueue.empty():
+#         laser = laserQueue.get(False)
+#         for blocker in blockerMirrorArray:
+#             col = Collision.Collision(blockerMirrorArray[1], laser)
+#             tempLaser, collissionState = col.collisionDetection(img)
+#             if collissionState == 1:
+#                 laserQueue.put(tempLaser)
+#                 collidedLasers.append(tempLaser)
+#
+# # for i in range(1):
+# #     for j in range(len(blockerMirrorArray)):
+# #         col = Collision.Collision(blockerMirrorArray[1], laserArray[i])
+# #         tempLaser, collissionState = col.collisionDetection(img)
+# #         if collissionState == 1:
+# #             laserArray.append(tempLaser)
+# #             colidedLasers.append(tempLaser)
 
 cap = cv2.VideoCapture(0)
 
