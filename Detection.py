199c65db5e06f298fccb9fd62f711816bb7c22f1
Yeehaaman <36846857+Yeehaaman@users.noreply.github.com>
2018-12-06 11:53:05 +0100

    Thresholding

--- Detection.py
+++ Detection.py
@@ -78,6 +78,10 @@ def detectionRed(clean_frame):
     blur = ImageProcessingMethods.ourMedianBlur(hsv)
 
     # Red
+    #lower_red = np.array[0, 120, 150]
+    #upper_red = np-array[5, 255, 255]
+    #mask_red = cv2.inRange(blur, lower_red, upper_red)
+
     lower_redH = np.array([0])
     upper_redH = np.array([5])
     lower_redS = np.array([120])
@@ -104,6 +108,10 @@ def detectionBlue(clean_frame):
     blur = ImageProcessingMethods.ourMedianBlur(hsv)
 
     # Blue
+    #lower_blue = np.array[100, 90, 90]
+    #upper_blue = np.array[115, 255, 255]
+    #mask_blue = cv2.inRange(mask_blue, lower_blue, upper_blue)
+
     lower_blueH = np.array([100])
     upper_blueH = np.array([115])
     lower_blueS = np.array([90])
