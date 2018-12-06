733f8485d8218f806303ef5baefc3dfd73637d11
HamPaa35 <mfaldt17@student.aau.dk>
2018-12-06 14:36:46 +0100

    Commented in the code

--- ImageProcessingMethods.py
+++ ImageProcessingMethods.py
@@ -39,14 +39,19 @@ def ourMedianBlur(img):
     return newImg
 
 
+# Method that applies thresholding to a HSV image
 def threshold(frame, lowerValueH, upperValueH, lowerValueS, upperValueS, lowerValueV, upperValueV):
+    # Retrieves the size of the image and then makes an output image in the right dimensions
     height = frame.shape[0]
     width = frame.shape[1]
     newImg = np.zeros((height, width))
+
+    # Loops through all the pixels in the the image, to apply the threshold
     for i in range(height - 1):
         for j in range(width - 1):
+            # Gets the color values of the image
             h, s, v = frame[i, j]
-
+            # Checks i the color value is within the threshold, and makes the pixel white if it is
             if upperValueH > h > lowerValueH and upperValueS > s > lowerValueS and upperValueV > v > lowerValueV:
                 newImg[i, j] = 255
             else:
@@ -55,13 +60,15 @@ def threshold(frame, lowerValueH, upperValueH, lowerValueS, upperValueS, lowerVa
 
 # dilation in a 5x5 circular kernel
 def dilation(img):
+    # Retrieves the size of the image and then makes an output image in the right dimensions
     height = img.shape[0]
     width = img.shape[1]
     color = [0] * 13
     newImg = np.zeros((height, width), np.uint8)
+    # Loops through all the pixels in the the image, to apply the dilation
     for i in range(height - 2):
         for j in range(width-2):
-
+            # The kernel is applied to the current pixel
             color[0] = img[i - 2][j]
 
             color[1] = img[i - 1][j - 1]
@@ -80,6 +87,7 @@ def dilation(img):
 
             color[12] = img[i + 2][j]
 
+            # If one of the pixels in the kernel is white, the current pixel is made white, else it will be black
             if color[0] or color[1] or color[2] or color[3] or color[4] or color[5] or color[6] or color[7] or color[8] \
                     or color[9] or color[10] or color[11] or color[12]:
                 newImg[i,j] = 255
@@ -91,13 +99,15 @@ def dilation(img):
 
 # Erosion in a 5x5 circular kernel
 def erosion(img):
+    # Retrieves the size of the image and then makes an output image in the right dimensions
     height = img.shape[0]
     width = img.shape[1]
     color = [0] * 13
     newImg = np.zeros((height, width), np.uint8)
+    # Loops through all the pixels in the the image, to apply the erosion
     for i in range(height - 2):
         for j in range(width - 2):
-
+            # The kernel is applied to the current pixel
             color[0] = img[i - 2][j]
 
             color[1] = img[i - 1][j - 1]
@@ -116,6 +126,7 @@ def erosion(img):
 
             color[12] = img[i + 2][j]
 
+            # If all of the pixels in the kernel is white, the current pixel is made white, else it will be black
             if color[0] and color[1] and color[2] and color[3] and color[4] and color[5] and color[6] and color[7] and color[8] \
                     and color[9] and color[10] and color[11] and color[12]:
                 newImg[i,j] = 255
@@ -125,12 +136,14 @@ def erosion(img):
     return newImg
 
 def closing(img):
+    # The dilation and erosion is applied to close the image
     dilatedIMG = dilation(img)
     newIMG = erosion(dilatedIMG)
 
     return newIMG
 
 def opening(img):
+    # The erosion and dilation is applied to close the image
     erodedIMG = erosion(img)
     newIMG = dilation(erodedIMG)
 
